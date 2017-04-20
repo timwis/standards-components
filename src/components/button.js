f2b97b244f7adac88c8d56175c6f17f5a07d91db
timwis <tim@timwis.com>
2017-04-20 07:51:43 -0400

    use unknown element instead of custom elements

--- src/components/button.js
+++ src/components/button.js
@@ -1,23 +1,19 @@
 const classNames = require('classnames')
 
-class PHLButton extends window.HTMLElement {
-  connectedCallback () {
-    const href = this.getAttribute('href')
-    const icon = this.getAttribute('icon')
-    const contents = this.innerText
-    const classes = this.className.split(' ')
-    const combinedClasses = classNames('button', 'icon', classes)
+module.exports = function PHLButton (el) {
+  const href = el.getAttribute('href')
+  const icon = el.getAttribute('icon')
+  const contents = el.innerText
+  const classes = el.className.split(' ')
+  const combinedClasses = classNames('button', 'icon', classes)
 
-    const template = `
-      <a href=${href} class="${combinedClasses}">
-        <div class="valign">
-          ${icon ? `<i class="fa ${icon} valign-cell"></i>` : ''}
-          <div class="button-label valign-cell">${contents}</div>
-        </div>
-      </a>
-    `
-    this.innerHTML = template
-  }
+  const template = `
+    <a href=${href} class="${combinedClasses}">
+      <div class="valign">
+        ${icon ? `<i class="fa ${icon} valign-cell"></i>` : ''}
+        <div class="button-label valign-cell">${contents}</div>
+      </div>
+    </a>
+  `
+  el.innerHTML = template
 }
-
-module.exports = PHLButton
